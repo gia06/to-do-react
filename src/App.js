b5a7c5d4b6c99755e75b90f45a665cdd82aa049f
gia <gia6shamugia@gmail.com>
2023-01-24 11:26:53 -0800

    added inout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import "./App.css";
 import { useEffect, useState } from "react";
 import bgLight from "./assets/bg-desktop-light.jpg";
 import bgDark from "./assets/bg-desktop-dark.jpg";
 import styled from "styled-components";
 import axios from "axios";
+import sunIcon from "./assets/icon-sun.svg";
+import moonIcon from "./assets/icon-moon.svg";
 
 function App() {
   const [isDarkTheme, setIsDarkTheme] = useState(false);
@@ -11,8 +12,8 @@ function App() {
   const api = process.env.REACT_APP_API;
 
   const fetchData = async () => {
-    const i = await axios.get(`${api}/toDos`);
-    console.log(i);
+    // const i = await axios.get(`${api}/toDos`);
+    // console.log(i);
   };
 
   useEffect(() => {
@@ -21,17 +22,56 @@ function App() {
 
   return (
     <>
-      <Img src={isDarkTheme ? bgDark : bgLight} />
+      <BackgroundImg src={isDarkTheme ? bgDark : bgLight} />
+      <Main>
+        <Header>
+          <Title>TODO</Title>
+        </Header>
+
+        <Input type="text" />
+      </Main>
     </>
   );
 }
 
 export default App;
 
-const Background = styled.img`
+const BackgroundImg = styled.img`
   width: 100vw;
   height: 300px;
+  z-index: 1;
   @media (max-width: 375px) {
     height: 200px;
   }
+`;
+
+const Main = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  position: absolute;
+  top: 70px;
+  left: 450px;
+  width: 60%;
+  max-width: 540px;
+  min-width: 327px;
+`;
+
+const Header = styled.div``;
+
+const Title = styled.h1`
+  height: 50px;
+  color: #ffffff;
+  text-align: left;
+  font-size: 40px;
+  font-height: 40px;
+  font-weight: 700;
+  letter-spacing: 15px;
+`;
+
+const Input = styled.input`
+  width: 100%;
+  height: 64px;
+  border-radius: 5px;
+  box-shadow: 0px 35px 50px -15px rgba(194, 195, 214, 0.5);
 `;
