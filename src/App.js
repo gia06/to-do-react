389aaf8c2353aa1e42db67062853cf829f1c0db3
gia <gia6shamugia@gmail.com>
2023-01-24 06:36:44 -0800

    modified font

--- src/App.js
+++ src/App.js
@@ -1,19 +1,37 @@
-import './App.css';
-import { useEffect, useState } from 'react';
-import bgLight from './assets/bg-desktop-light.jpg'
+import "./App.css";
+import { useEffect, useState } from "react";
+import bgLight from "./assets/bg-desktop-light.jpg";
+import bgDark from "./assets/bg-desktop-dark.jpg";
+import styled from "styled-components";
+import axios from "axios";
 
 function App() {
-  const [state, setState] = useState(false)
+  const [isDarkTheme, setIsDarkTheme] = useState(false);
+
+  const api = process.env.REACT_APP_API;
+
+  const fetchData = async () => {
+    const i = await axios.get(`${api}/toDos`);
+    console.log(i);
+  };
 
   useEffect(() => {
-
-  }, [])
+    fetchData();
+  }, []);
 
   return (
-    <div className='bg-light'>
-      <img src={bgLight} alt="background image"/>
-    </div>
+    <>
+      <Img src={isDarkTheme ? bgDark : bgLight} />
+    </>
   );
 }
 
 export default App;
+
+const Background = styled.img`
+  width: 100vw;
+  height: 300px;
+  @media (max-width: 375px) {
+    height: 200px;
+  }
+`;
